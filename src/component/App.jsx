f8dd9856a8614753dc3b0c00d8764b45b55822cc
viki-119 <1192756137@qq.com>
2016-04-24 11:20:04 +0800

    Create App.jsx

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+import './App.less';
+import FormAnt from './myapp/FormAnt';
+
+const App = () =>
+  <FormAnt />
+;
+
+
+export default App;
+
