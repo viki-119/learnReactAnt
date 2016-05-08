2681a0c5a133ff75683bf35e1da57d1476b91c75
viki-119 <1192756137@qq.com>
2016-05-08 13:31:26 +0800

    update

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,18 +1,6 @@
 import '../common/lib';
+import App from '../component/App';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Router } from 'react-router';
-import App from '../component/App';
-// import FormAnt from '../component/myApp/FormAnt';
-import SelectAnt from '../component/myApp/SelectAnt';
 
-const routes = {
-  path: '/',
-  component: App,
-  childRoutes: [
-    { path: 'index', component: SelectAnt },
-    { path: 'about', component: App },
-  ],
-};
-
-ReactDOM.render(<Router routes= {routes} />, document.getElementById('react-content'));
+ReactDOM.render(<App />, document.getElementById('react-content'));
